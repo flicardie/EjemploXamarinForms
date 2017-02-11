0dad7434767c494564944932d9f8ba00988ffeec
flicardie <flicardie@gmail.com>
2017-02-10 22:47:56 -0600

    Nuevo Ejemplo de Contenedor

---
+++ EjemploXamarinForms/EjemploXamarinForms/Pages/ContentPageWithToolbar.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EjemploXamarinForms.Pages
+{
+    public partial class ContentPageWithToolbar : ContentPage
+    {
+        public ContentPageWithToolbar()
+        {
+            InitializeComponent();
+        }
+
+        void OnAddItemClicked(object sender, EventArgs e)
+        {
+            //var todoItem = new TodoItem()
+            //{
+            //    ID = Guid.NewGuid().ToString()
+            //};
+            //var todoPage = new TodoItemPage(true);
+            //todoPage.BindingContext = todoItem;
+            //Navigation.PushAsync(todoPage);
+        }
+    }
+}
